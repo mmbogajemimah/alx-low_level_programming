933fc334e4580b41212b2695af4bf3e4df9de0dc
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-07 04:34:34 -0700

    Print without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - entry point
+ *
+ * Return: returns 1 for reasons known only to the ALX gods
+ */
+#include <stdio.h>
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(s, stderr);
+	return (1);
+}
