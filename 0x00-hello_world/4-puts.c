0bd5617cb8d092dec278816e28e9cd433b7ec98c
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-07 03:04:56 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 #include <string.h>
 
 int main (void)
 {
-	char str1[50];
-	strcpy(str1, "Programming is like building a multilingual puzzle");
+	char str1[50]="Programming is like building a multilingual puzzle";
 	puts(str1);
 	return (0);
 }
