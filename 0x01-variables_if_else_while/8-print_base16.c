b0902a84cb8af4b9f78d517ff4602883ae2196cb
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-08 14:07:59 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 	char ch;
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 		putchar(i % 10 + '0');
-	for(ch = 'a'; ch <= 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
