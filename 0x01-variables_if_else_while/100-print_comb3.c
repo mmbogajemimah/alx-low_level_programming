0bbff961f12cbf39496899a93250477ca18ffbd5
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-08 14:57:42 -0700

    Double Digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i % 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 9 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
