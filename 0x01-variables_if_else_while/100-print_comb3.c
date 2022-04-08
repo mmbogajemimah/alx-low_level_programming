a5d91f7e27560fa0628592d8ffb4101b6017bdd5
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-08 15:17:59 -0700

    Advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			putchar(i % 10 + '0');
 			putchar(j % 10 + '0');
-			if (i == 9 && j == 9)
+
+			if (i == 8 && j == 9)
+			{
 				continue;
+			}
 			putchar(',');
-			putchar(' ');	
+			putchar(' ');
 		}
 	}
 	putchar('\n');
