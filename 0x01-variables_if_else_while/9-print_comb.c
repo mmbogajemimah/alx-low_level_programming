caed7e1809fd3bbd2dd7451806954c669ba3776f
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-08 14:17:19 -0700

    Single Digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
