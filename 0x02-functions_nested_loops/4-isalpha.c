3e1d6eda7c749588440b3a5405f7609518e9ded8
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-12 07:57:05 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetical letters
+ * @c: The character to be checked
+ *
+ * Return: returns 1 if is alpha and 0 if is not alpha
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
