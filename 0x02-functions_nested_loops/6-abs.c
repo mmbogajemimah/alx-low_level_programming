eb8be10b89823a4a91dabac68e8ffefd9896db48
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-12 08:37:21 -0700

    Absolute Values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Transforms negative digits to positive
+ * @n: Integer provided to the function
+ *
+ * Return: Returns the absolute value
+ */
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
