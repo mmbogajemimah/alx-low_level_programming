b1c016f965bf2b6212a64657e270f999ac2b98e4
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-13 08:45:26 -0700

    Natural Numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-    int i;
-    int x;
-    
-    for (i = 0; i < 1024; i++)
-    {
-        if(i % 3 == 0 || i % 5 == 0)
-        {
-            x = x + i;
-        }
-    }
-    printf("%d\n", x);
-    return (0);
+	int i;
+	int x;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			x = x + i;
+		}
+	}
+	printf("%d\n", x);
 }
