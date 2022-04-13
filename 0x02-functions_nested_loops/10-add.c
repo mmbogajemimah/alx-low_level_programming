e3c9338abd5eb9c1ffafa50d779a35902dd2a11c
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-13 00:23:07 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Adds Integers
+ * @x: Integer one
+ * @y: Integer two
+ *
+ * Return: returns the result of adding the two digits
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
