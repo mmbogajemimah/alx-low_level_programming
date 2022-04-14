4b15f652eb24816c4c1232dd3309b712d60ff006
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-14 05:39:25 -0700

    Number Exclusion

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints numbers between 0 and 10 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
