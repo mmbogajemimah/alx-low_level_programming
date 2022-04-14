7f65afe64e7060ad6ab05174e9090840bd624ecc
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-14 06:46:36 -0700

    Diagonal Triangle

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - prints vertical line in form of a Triangle
+ * @n: number of vertical lines printed
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+}
