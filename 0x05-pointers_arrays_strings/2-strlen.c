1e36f9d03e2b50bbde2cf14ec7dd41e9eeedeb53
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-20 05:22:48 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - Calculates the length of s string
+ * @s: String which length is to be found
+ *
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
