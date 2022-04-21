8b956f5e6ece4858be6aef811d1ab637c136e2c2
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-21 00:14:39 -0700

    Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+	return (0);
+}
