3fe7704665ae076b356ad4c7368e66eeabab068a
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-24 03:26:30 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
- * leet - Switches letters in string
- * @c: String passed in function
- *
- * Return: returns string with switched strings
+ * leet - replaces specific characters with specific numbers
+ * @c: string to do the transformation
+ * Return: transformed string
  */
 char *leet(char *c)
 {
-	int i, j;
-	char s1[] = "AEOLT";
-	char s2[] = "aeolt";
-	char s3[] = "43071";
+	char l1[] = "AEOLT";
+	char l2[] = "aeolt";
+	char l3[] = "43017";
+	unsigned int i, j;
 
-	for (i = 0; c[i] != '\0'; i++)
+	for (i = 0; i < strlen(c); i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (c[i] == s1[j] || c[i] == s2[j])
-			{
-				c[i] = s3[j];
-				break;
-			}
+			if ((c[i] == l1[j]) || (c[i] == l2[j]))
+				c[i] = l3[j];
 		}
 	}
+
 	return (c);
 }
