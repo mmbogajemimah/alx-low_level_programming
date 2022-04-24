5487c3fdf7c30617246873b44e56d11772d980ce
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-24 03:21:37 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,18 +9,19 @@
  */
 char *leet(char *c)
 {
-	unsigned int i, j;
+	int i, j;
 	char s1[] = "AEOLT";
 	char s2[] = "aeolt";
 	char s3[] = "43071";
 
-	for (i = 0; i < strlen(c); i++)
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		for (j = 0; j < strlen(s1); j++)
+		for (j = 0; j < 5; j++)
 		{
 			if (c[i] == s1[j] || c[i] == s2[j])
 			{
 				c[i] = s3[j];
+				break;
 			}
 		}
 	}
