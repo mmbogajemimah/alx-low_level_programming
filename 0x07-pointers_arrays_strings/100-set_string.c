44e04ce144d933f13b74b18d39ce4ca4e1456d58
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-26 13:35:18 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - Replaces string
+ * @s: pointer to pointer
+ * @to: pointer
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
