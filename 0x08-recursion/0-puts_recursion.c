f1f859eb0811f5d440140cbc304981386dff5f68
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-04-28 07:10:14 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string
+ * @s: argument string
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
