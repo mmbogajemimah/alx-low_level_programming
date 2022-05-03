658e46a44522d9013b9c91cf4200cea906f95012
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-03 07:18:39 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Starting Point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Returns 0 when program is sucessful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	_putchar((argc - 1) + '0');
+	_putchar('\n');
+	return (0);
+}
