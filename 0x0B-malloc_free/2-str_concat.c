5ca8b35d102db2566c343441d1dbb86ccbfb233e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-06 03:13:45 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int len = 0;
+	char *str = NULL;
+	int i;
+
 	if (s1 == NULL || s2 == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		return (strcat(s1, s2));
+		len = strlen(s1) + strlen(s2);
+		str = (char *)malloc(sizeof(char) * len);
+
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			str[i] = s1[i];
+		}
+
+		for (j = 0; s2[j] != '\0'; j++, i++)
+		{
+			str[i] = s2[j];
+		}
+		str[i] = '\0';
 	}
+	return (str);
 }
