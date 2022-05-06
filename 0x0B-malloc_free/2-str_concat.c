0ac2cde9eb6c28e88029e7f36f792be83f142ce1
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-06 03:32:07 -0700

    Concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i, j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	else
 	{
