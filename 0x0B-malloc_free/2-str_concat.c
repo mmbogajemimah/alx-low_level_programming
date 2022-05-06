c0cd7239090df61d62d2ed7fe8aec70a30421de6
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-06 03:54:23 -0700

    Concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,52 @@
-#include "main.h"
+include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "holberton.h"
+
 /**
- * str_concat - a funcion that concatnates strings
- * @s1: string one
- * @s2: string two
- *
- * Return: Returns concatnated string
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
+
+
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	int len = 0;
-	char *str;
-	int i, j;
+int size1, size2, i;
+char *m;
 
-	if (s1 == NULL)
-	{
-		s1 = "\0";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "\0";
-	}
+if (s1 == NULL)
+	s1 = "\0";
+if (s2 == NULL)
+	s2 = "\0";
+
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+m = malloc((size1 + size2) *sizeof(char) + 1);
+if (m == 0)
+	return (0);
+
+for (i = 0; i <= size1 + size2; i++)
+{
+	if (i < size1)
+		m[i] = s1[i];
 	else
-	{
-		len = (strlen(s1) + strlen(s2) + 1);
-		str = malloc(sizeof(char) * len);
-
-		if (str == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (i = 0; i < (strlen(s1) + strlen(s2)); i++)
-			{
-				if (i < strlen(s1))
-					str[i] = s1[i];
-				else
-					str[i] = s2[i - strlen(s1)]
-			}
-			str[i] = '\0';
-		}
-	}
-	return (str);
-	free(str);
+		m[i] = s2[i - size1];
+}
+m[i] = '\0';
+return (m);
 }
