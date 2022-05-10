e595318bb46c168e3f8c44f079fa2205055d3961
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-10 11:46:01 -0700

    Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,44 +12,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int string1, string2, length, j, i;
-	char *str;
+	unsigned int s1_len, tot_len, i;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	string1 = strlen(s1) + 1;
-	string2 = strlen(s2) + 1;
-	length = string1 + string2;
 
-	str = malloc((sizeof(char) * length));
-	if (str == NULL)
-	{
-		free(str);
+	s1_len = strlen(s1);
+	tot_len = s1_len + n;
+
+	s = malloc((tot_len + 1) * sizeof(char));
+
+	if (s == NULL)
 		return (NULL);
-	}
-	if (n > string2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			str[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			str[i] = s2[j];
-			i++;
-		}
-	}
-	else if (n < string2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			str[i] = s1[i];
-		for (j = 0; j < n; j++)
-		{
-			str[i] = s2[j];
-			i++;
-		}
-	}
-	str[i + 1] = '\0';
-	return (str);
-	free(str);
+
+	for (i = 0; i < s1_len; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i < n && s2[i]; i++)
+		s[s1_len + i] = s2[i];
+
+	s[tot_len] = '\0';
+	return (s);
+	free(s);
 }
