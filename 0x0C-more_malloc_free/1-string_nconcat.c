4620839517050d372103aaf50eab84c03e5dece4
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-10 09:15:50 -0700

    Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int string1, string2, length, j, i;
 	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	string1 = strlen(s1) + 1;
 	string2 = strlen(s2) + 1;
 	length = string1 + string2;
