d602504cb8292bb938eb09e504ea2f7f9a293092
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-12 11:15:22 -0700

    Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main program
+ *
+ * Return: returns 0.
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef abs
+#define abs
+#define ABS(x) ((x < 0) ? (-x) : (x))
+
+#endif /*ABS*/
