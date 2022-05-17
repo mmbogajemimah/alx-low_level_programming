8f867f635cf2c4a13b0fec7d7ad88ef432275450
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-17 15:41:25 -0700

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator == NULL)
 			separator = "";
 		if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		if (i == n - 1)
 			printf("%s", str);
 		else
