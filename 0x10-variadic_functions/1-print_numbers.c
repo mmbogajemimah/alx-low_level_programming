275c5a3b8571ad2401404f40b21fdfcc1fa654cf
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-17 12:57:53 -0700

    a function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		return;
 	}
-	if (separator == NULL)
-		separator = "";
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
