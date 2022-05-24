594bda7a31d775bceba770e48bfa9a092e185619
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-05-24 14:51:47 -0700

    Printf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "100-main.c"
+/**
+ * print - runs before main
+ * Return: Nothing
+ */
+void print()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	main();
+}
