c34662184888de10d25e902a82c91b9f1f97fc53
Jemimah Mmboga <mboga jemimah@gmail.com>
2022-07-11 22:27:21 +0300

    Dynamic Libs

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - returns absolute value of the number input
+ * @x: number to be absoluted
+ *
+ * Return: returns absolute value
+ */
+int _abs(int x)
+{
+	return (abs(x));
+}
