15733e932c89ecb37d62e374cad698b003012266
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-09-15 12:36:07 -0700

    Makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""
-'island_perimeter' module
-"""
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
-    returns the perimeter of the island described in `grid` where:
-        - 0 represents a water zone
-        - 1 represents a land zone
-        - one cell is a square with side length 1
-        - grid cells are connected horizontally or vertically but not diagonally
-        - grid is rectangular, and width and height don't exceed 100
-    """
-    pass
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
