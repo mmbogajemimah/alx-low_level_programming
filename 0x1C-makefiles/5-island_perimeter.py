9e3e1df239f0e3aa7ae4380c2d94594ffeada897
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-09-15 12:31:57 -0700

    MakeFile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+'island_perimeter' module
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in `grid` where:
+        - 0 represents a water zone
+        - 1 represents a land zone
+        - one cell is a square with side length 1
+        - grid cells are connected horizontally or vertically but not diagonally
+        - grid is rectangular, and width and height don't exceed 100
+    """
+    pass
