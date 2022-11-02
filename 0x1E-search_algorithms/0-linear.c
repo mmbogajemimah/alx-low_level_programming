db816dfac2f051d99843d14a5da11fa7a5907152
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-02 13:55:58 -0700

    a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,11 @@
  * @value: Value to search for
  *
  * Return: Returns index of the value
+ *
+ * Description: Prints the values of the array anytime they get compared
  */
+
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
@@ -19,7 +23,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
